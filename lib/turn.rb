ced77a82b44a99135b5afff3f745f4b188109c9a
Kevin Boston <kbos22@gmail.com>
2020-04-28 18:13:45 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,4 +1,3 @@
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -7,40 +6,34 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def input_to_index(input)
-  input.to_i - 1
+def input_to_index(user_input)
+  user_input.to_i - 1
 end
 
 def valid_move?(board, index)
   if index.between?(0,8)
-    if position_taken?(board, index)
-      TRUE
+    if !position_taken?(board, index)
+      true
     end
   end
 end
 
 def turn(board)
-  puts "Welcome to Tic Tac Toe!"
-  display_board(board)
   puts "Please enter 1-9:"
   input = gets.strip
   index = input_to_index(input)
-  if validmove?(board, index)
+  if valid_move?(board, index)
     move(board, index)
     display_board(board)
   else
     turn(board)
   end
-  
 end
-
-
-
 
 def position_taken?(board, index)
   board[index] != " "
 end
 
-def move(board, index, char = "X")
-  board[index] = char
+def move(board, index, current_player = "X")
+  board[index] = current_player
 end
